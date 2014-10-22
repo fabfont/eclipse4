e5f551c1cf80971a880bb7d6e93cd8b74e6de0df
fabfont <T0121984@localhost.localdomain>
2014-10-22 18:09:11 +0200

    Add javadoc 2

--- src/fr/ffontenoy/eclipse4/empty/handler/SysoutHandler.java
+++ src/fr/ffontenoy/eclipse4/empty/handler/SysoutHandler.java
@@ -5,6 +5,9 @@ import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.extensions.Preference;
 
+/**
+ * Just for printing something 
+ */
 public class SysoutHandler {
 	@Execute
 	public void execute(@Preference(nodePath = "fr.ffontenoy.eclipse4.empty") IEclipsePreferences prefs) {
